c238fed1182aaac0816677cb3bc5f95c372b8546
Strappazzon <strappazzon@protonmail.com>
2020-03-17 20:14:25 +0100

    Don't disable X button but cancel exit instead

--- src/Windows Activation Client/Verification.vb
+++ src/Windows Activation Client/Verification.vb
@@ -1,24 +1,13 @@
 ﻿Public Class Verification
-    Private Const MF_BYPOSITION As Integer = &H400
-    Private Const MF_REMOVE As Integer = &H1000
-    Private Declare Auto Function GetSystemMenu Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal bRevert As Integer) As IntPtr
-    Private Declare Auto Function GetMenuItemCount Lib "user32.dll" (ByVal hMenu As IntPtr) As Integer
-    Private Declare Function DrawMenuBar Lib "user32.dll" (ByVal hWnd As IntPtr) As Boolean
-    Private Declare Auto Function RemoveMenu Lib "user32.dll" (ByVal hMenu As IntPtr, ByVal nPosition As Integer, ByVal wFlags As Integer) As Integer
     Private WithEvents VerificationTimer As New Timer()
 
-    Private Sub DisableCloseButton(ByVal hWnd As IntPtr)
-        '//stackoverflow.com/a/38311385
-        Dim hMenu As IntPtr
-        Dim n As Integer
-        hMenu = GetSystemMenu(hWnd, 0)
-        If Not hMenu.Equals(IntPtr.Zero) Then
-            n = GetMenuItemCount(hMenu)
-            If n > 0 Then
-                RemoveMenu(hMenu, n - 1, MF_BYPOSITION Or MF_REMOVE)
-                RemoveMenu(hMenu, n - 2, MF_BYPOSITION Or MF_REMOVE)
-                DrawMenuBar(hWnd)
-            End If
+    Private Sub Verification_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If e.CloseReason = CloseReason.UserClosing Then
+            'Don't close the form
+            e.Cancel = True
+
+            MessageBox.Show("Are you sure you want to cancel the verification process?", "Windows Activation", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+            Cursor = Cursors.WaitCursor
         End If
     End Sub
 
@@ -43,9 +32,6 @@
     End Sub
 
     Private Sub Verification_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Disable the close button
-        DisableCloseButton(Handle)
-
         If Form2.ProductKeyTextBox.Text = "5T0PW-4ST1N-GURT1-M35C4-MM1NG" Then
             VerificationTimer.Interval = 100
             VerificationTimer.Start()
